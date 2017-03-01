adba7051e1acc848bc58dcc114aec319867018e8
alvaroamoreno <alvaro.angel@hotmail.es>
2017-03-01 23:41:40 +0100

    FINAL P1

--- PAlgo/DAlgo/src/P1/biseccion.java
+++ PAlgo/DAlgo/src/P1/biseccion.java
@@ -1,30 +1,28 @@
 package P1;
 
-import javax.script.ScriptEngine;
 import java.util.Stack;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+
 
 public class biseccion {
-	
-	public static long recur;
-	public static long stack;
 	
 	public static void main(String [] args) {
 		double result;
 		double error = 10e-4;
 		String eq = "(X*X*X)+10*X - 5";
+		long t1,t2,t3;
+		
+		t1 = System.nanoTime();
 		result = recurbisec(0,1,error);
+		t2 = System.nanoTime();
 		result = stackbisec (0,1,error);
+		t3 = System.nanoTime();
 		
 		System.out.println("The root of " + eq + " is: " + result);
-		System.out.println("Para el método recursivo llevó: " + recur + " ns, para el de pilas: " + stack + " ns.");
-		System.out.println("Para el método recursivo llevó: " + recur/10e6 + " ms, para el de pilas: " + stack/10e6 + " ms.");
+		System.out.println("Para el método recursivo llevó: " + (t2-t1) + " ns, para el de pilas: " + (t3-t2) + " ns.");
+		System.out.println("Para el método recursivo llevó: " + (t2-t1)/10e6 + " ms, para el de pilas: " + (t3-t2)/10e6 + " ms.");
 	}
 
 	static double recurbisec (double a, double b, double error) {		//Complejidad T(n-1) + T(n-2), n el resto de casos.
-		long t1,t2;
-		t1 = System.nanoTime();
 		double valormedio = (a+b)/2;
 		double sol = 0;
 		if (Math.abs(eval(valormedio)) <= error){		//base case
@@ -38,15 +36,10 @@ public class biseccion {
 				sol = recurbisec (a,valormedio,error);	
 		}
 
-		t2 = System.nanoTime();
-		recur = t2 - t1;
 		return sol;		
 	}
 
 	static double stackbisec (double a, double b, double error) {		//Complejidad O(n); Va de 0 a n (n es la iteración en la que se cumple eval(p) <= error
-		long t1,t2;
-		t1 = System.nanoTime();
-		
 		Stack <Double> left = new Stack <Double>();
 		Stack <Double> right = new Stack <Double>();
 		
@@ -65,8 +58,7 @@ public class biseccion {
 			}
 			p = (left.peek() + right.peek())/2.0;
 		}
-		t2 = System.nanoTime();
-		stack = t2 - t1;
+
 		return p;
 	}
 
